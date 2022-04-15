7c510e2eba740230ba148b9d2ffaced1b9b111a9
Manmohit Matharu <manmohit.matharu@gmail.com>
2022-04-14 23:25:18 -0400

    finally done

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,19 +1,21 @@
 const request = require('request');
-const userInput = process.argv[2];
 
-request(`https://api.thecatapi.com/v1/breeds/search?q=${userInput}`, function(error, response, body) {
-  if (body) {
-    body = JSON.parse(body);
-    if (body[0] === undefined) {
-      console.log("No cat exists"); // connects to site but no breed with the argument found
-    } else {
-      console.log(body[0].description); // connects to site and returns breed description based on argument
+const fetchBreedDescription = (breedName, callback) => {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, function(error, response, body) {
+    if (body) {
+      body = JSON.parse(body);
+      if (body[0] === undefined) {
+        errorMsg = "No cat exists";
+        return errorMsg; // connects to site but no breed with the argument found
+      } else {
+        return  callback(null, body[0].description); // connects to site and returns breed description based on argument
+      }
     }
-  }
 
-  if (error) {
-    console.log('wrong url'); // changed URL and throws an error if URL doesn't work
-  }
+    if (error) {
+      return callback(error, null); // changed URL and throws an error if URL doesn't work
+    }
+  });
+};
 
-  
-});
+module.exports = { fetchBreedDescription };
